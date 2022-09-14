6c0f2974f0ea148a8eb40ded6f276be7b1a7d8e6
Ingo Oppermann <ingo@oppermann.ch>
2022-09-14 14:49:03 +0200

    Increase RTMP client compatibility

--- examples/rtmp_publish_sink/main.go
+++ examples/rtmp_publish_sink/main.go
@@ -69,6 +69,9 @@ func (s *server) log(who, action, path, message string, client net.Addr) {
 }
 
 func (s *server) handlePlay(conn *rtmp.Conn) {
+	client := conn.NetConn().RemoteAddr()
+
+	s.log("PLAY", "INVALID", conn.URL.Path, "not supported", client)
 	conn.Close()
 }
 
@@ -108,15 +111,23 @@ func main() {
 	var cert string
 	var key string
 	var help bool
+	var addr string
+	var app string
+	var token string
 
 	flag.StringVar(&cert, "cert", "", "Path to the certifacate file")
 	flag.StringVar(&key, "key", "", "Path to the key file")
+	flag.StringVar(&addr, "addr", ":1935", "Address to listen on")
+	flag.StringVar(&app, "app", "/", "RTMP app, should start with /")
+	flag.StringVar(&token, "token", "", "Token query string")
 	flag.BoolVar(&help, "h", false, "Show options")
 
 	flag.Parse()
 
 	config := Config{
-		Addr: ":1935",
+		Addr:  addr,
+		App:   app,
+		Token: token,
 	}
 
 	server, _ := New(config)
